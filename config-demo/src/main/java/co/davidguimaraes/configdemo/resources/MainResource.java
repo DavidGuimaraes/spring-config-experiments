dce7f36da950290fb71f9e76d554420a212fbeb7
David Guimaraes <david.ce@live.com>
2020-03-26 18:41:03 -0400

    13. Activating refresh for real-time configs management.

--- config-demo/src/main/java/co/davidguimaraes/configdemo/resources/MainResource.java
+++ config-demo/src/main/java/co/davidguimaraes/configdemo/resources/MainResource.java
@@ -3,6 +3,7 @@ package co.davidguimaraes.configdemo.resources;
 import co.davidguimaraes.configdemo.config.DbSettings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Map;
 
+@RefreshScope
 @RestController
 @RequestMapping("/rest")
 public class MainResource {
