8748ef46b33410cf270ca6c2d1be5e4a3f030f70
David Guimaraes <david.ce@live.com>
2020-03-26 13:51:50 -0400

    02. Experiment 1 - Using property file inside the Jar file and pulling values with @Value annotation.

---
+++ config-demo/src/main/java/co/davidguimaraes/configdemo/resources/MainResource.java
@@ -0,0 +1,19 @@
+package co.davidguimaraes.configdemo.resources;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest")
+public class MainResource {
+
+    @Value("${app.name}")
+    private String appName;
+
+    @GetMapping("/hello")
+    public String getGreeting(){
+        return "Welcome to " + this.appName;
+    }
+}
